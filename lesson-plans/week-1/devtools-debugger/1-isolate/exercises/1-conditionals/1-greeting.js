e35e47b48a94457a94066d1e804d3aba896498e3
Pallavi Sarwar <pallavi.sarwar@gmail.com>
2024-02-29 11:33:11 +0100

    exercise 1-greetings  done

--- lesson-plans/week-1/devtools-debugger/1-isolate/exercises/1-conditionals/1-greeting.js
+++ lesson-plans/week-1/devtools-debugger/1-isolate/exercises/1-conditionals/1-greeting.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let input = prompt('enter your name:');
+/*let input = prompt('please enter your name:');
 
 let greeting = '';
 if (input === null) {
@@ -15,3 +15,18 @@ alert(greeting);
   - Ask the user for their first and last names.
   - Greet them with their full name.
 */
+
+let firstName = prompt('enter your first name: ');
+let lastName = prompt('enter your last name: ');
+
+fullName = firstName + ' ' + lastName;
+
+let greeting = '';
+
+if (firstName && lastName ){
+ greeting = 'hello' + ' ' + fullName + '!';
+} else {
+  greeting = 'hello anonymous.';
+}
+
+alert(greeting);
